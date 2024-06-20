7e851912053ca4b80ce1c823c1931618a3bd12ba
alexkivuva <alexkivuvan12@gmail.com>
2024-06-20 06:48:06 +0100

    add the R dataset

--- test.R
+++ test.R
@@ -5,3 +5,11 @@ install.packages("caret")
 install.packages("dplyr")
 
 install.packages("ggplot2")
+
+install.packages ("palmerpenguins")
+library(palmerpenguins)
+penguins%>%ggplot(aes(x=bill_depth_mm)) + geom_histogram()
+
+library(tidyverse)
+library(ggplot2)
+
